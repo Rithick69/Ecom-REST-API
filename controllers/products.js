149df346243d36cbc1e7f6343e363315db4beac3
Rithick69 <rithickcy19@gmail.com>
2024-01-07 00:31:02 +0530

    Added sample product Data to the database for testing and also created custom search functionality for the products in DB using regex

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,24 @@ const MyModel = require("../models/productSchema");
 
 
 const getAllProducts = async(req, res) => {
-    const myData = await MyModel.find(req.query);
+
+    const { company, name, featured } = req.query;
+
+    const queryObj = {};
+
+    if(company){
+        queryObj.company = { $regex: company, $options: "i"}
+    }
+
+    if(name){
+        queryObj.name = { $regex: name, $options: "i"}
+    }
+
+    if(featured){
+        queryObj.featured = { $regex: featured, $options: "i"}
+    }
+
+    const myData = await MyModel.find(queryObj);
     res.status(200).json({ myData});
 }
 
