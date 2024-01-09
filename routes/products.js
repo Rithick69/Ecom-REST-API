aff1521b1a7d25565d27755006100c67e6f20bd2
Rithick69 <rithickcy19@gmail.com>
2024-01-10 00:21:32 +0530

    Updated the product data for the Rest pi to suit the format expected in the ecommerce website.

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllProducts, getAllProductsTest } = require("../controllers/products");
+const { getAllProducts, getAllProductsTest, saveProducts } = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
+router.route("/save").post(saveProducts);
 router.route("/test").get(getAllProductsTest);
 
 module.exports = router;
