03bcc3d019bd7a3ce858e740027f8c26bbc30caf
Rithick69 <rithickcy19@gmail.com>
2024-01-13 23:49:11 +0530

    Created bcrypt encryption and jwt tokengenerations for login, register and logout requests. WIP

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const { loginUser, registerUser  } = require("../controllers/user_controller");
+const { loginUser, registerUser, refreshTokenFunc, logoutUser  } = require("../controllers/user_controller");
 
-router.route("/login").get(loginUser);
+router.route("/login").post(loginUser);
+router.route("/logout").delete(logoutUser);
+router.route("/refreshToken").post(refreshTokenFunc);
 router.route("/register").post(registerUser);
 
 module.exports = router;
