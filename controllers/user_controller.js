dc205f31611d61263abb635d38fba72a52b49d35
Rithick69 <rithickcy19@gmail.com>
2024-02-02 23:32:15 +0530

    Fixing the middleware errors

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,148 +1,141 @@
+const UserModel = require('../models/userSchema');
+const {
+	generateAccessToken,
+	generateRefreshToken,
+} = require('../util/tokenizer');
+const { cacheFunc, deleteToken, searchToken } = require('../util/cacher');
+const { REFRESH_TOKEN } = require('../config');
 
-const UserModel = require("../models/userSchema");
-const { generateAccessToken, generateRefreshToken } = require("../util/tokenizer");
-const { cacheFunc, deleteToken, searchToken } = require("../util/cacher");
-const { REFRESH_TOKEN } = require("../config");
+const refreshTokenFunc = async (req, res, next) => {
+	try {
+		const { token } = req.body;
 
-const refreshTokenFunc = async (req,res) => {
+		if (!searchToken(token)) {
+			return res.status(400).send('Refresh Token Invalid');
+		}
 
-    try{
-        const { token } = req.body;
+		await deleteToken(token);
 
-        if (!searchToken(token)){
-            return res.status(400).send("Refresh Token Invalid");
-        }
+		const userData = jwt.verify(token, REFRESH_TOKEN, (err, data) => {
+			if (err) {
+				const error = new Error('Token invalid');
+				error.status = 403;
+				throw error;
+				// return res.status(403).send("Token invalid")
+			} else {
+				return data;
+			}
+		});
 
-        deleteToken(token);
+		const accessToken = await generateAccessToken(userData);
+		const refreshToken = await generateRefreshToken(userData);
 
-        const userData = jwt.verify(token, REFRESH_TOKEN, (err, data) => {
-            if (err) {
-                const error = new Error("Token invalid");
-			    error.status = 403
-			    throw error;
-                // return res.status(403).send("Token invalid")
-            } else {
-                return data;
-            }
-        });
+		cacheFunc(refreshToken);
 
-        const accessToken = await generateAccessToken(userData);
-        const refreshToken = await generateRefreshToken(userData);
-
-        cacheFunc(refreshToken);
-
-        return res.json ({ accessToken: accessToken, refreshToken: refreshToken });
-
-    } catch (e){
-        console.log(e);
-        const message = "Failed to Refresh Session";
-        const details = e.message;
-        const status = e.status;
-        next({status, message, details});
-    }
-
+		return res.json({ accessToken: accessToken, refreshToken: refreshToken });
+	} catch (e) {
+		console.log(e);
+		const message = 'Failed to Refresh Session';
+		const details = e.message;
+		const status = e.status;
+		next({ status, message, details });
+	}
 };
 
-const updateUserDetails = async(req, res)=>{
-    try{
+const updateUserDetails = async (req, res, next) => {
+	try {
+		const userData = req.body;
 
-        const userData = req.body;
+		const query = { email: userData.email };
 
-        const query = {email: userData.email};
+		await UserModel.updateOne(query, userData.payload);
 
-        await UserModel.updateOne(query, userData.payload);
-
-        res.status(200).json({msg:"User Details updated"})
-
-    } catch (e){
-        console.log(e);
-    }
-}
-
-const logoutUser = async(req, res) => {
-    try{
-
-        deleteToken(req.body.token);
-
-        res.status(204).send("Logged out!")
-
-    } catch (e){
-        console.log(e);
-        const message = "Internal Server Error";
-        const details = e;
-        next({message, details});
-    }
-
+		res.status(200).json({ msg: 'User Details updated' });
+	} catch (e) {
+		console.log(e);
+	}
 };
 
+const logoutUser = async (req, res, next) => {
+	if (req.body.token) {
+		await deleteToken(req.body.token);
 
-
-const loginUser = async(req, res) => {
-
-    try{
-
-        const { email, password } = req.body;
-
-        const user = await UserModel.findOne({ email });
-
-        if ( !user ) {
-            res.status(400).send("User does not exist");
-        }
-
-        if ( await user.comparePassword(password) ) {
-            const accessToken = await generateAccessToken(user);
-            const refreshToken = await generateRefreshToken(user);
-
-            cacheFunc(refreshToken);
-
-            return res.status(200).json ({
-                msg: "Login Successful",
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                userId: user._id.toString()
-            });
-        }
-
-        return res.status(401).send("Invalid Email or Password!")
-
-    } catch (e){
-        console.log(e);
-        const message = "Internal Server Error";
-        const details = e;
-        next({message, details});
-    }
+		res.status(204).send("Logged out!");
+	} else {
+		const message = 'Internal Server Error';
+        const details = 'Refresh Token not recieved'
+		next({ message, details });
+	}
 };
 
-const registerUser = async(req, res) => {
-    try{
-        const { email } = req.body;
+const loginUser = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
 
-        const userExist = await UserModel.findOne({ email });
+		const user = await UserModel.findOne({ email });
 
-        if (userExist) {
-            return res.status(400).json({msg: "User already exists"});
-        }
+		if (!user) {
+			res.status(400).send('User does not exist');
+		}
 
-        const payload = {...req.body};
+		if (await user.comparePassword(password)) {
+			const accessToken = await generateAccessToken(user);
+			const refreshToken = await generateRefreshToken(user);
 
-        const userCreated = await UserModel.create(payload);
-        const refreshToken = await generateRefreshToken(userCreated);
+			cacheFunc(refreshToken);
 
-        cacheFunc(refreshToken);
+			return res.status(200).json({
+				msg: 'Login Successful',
+				accessToken: accessToken,
+				refreshToken: refreshToken,
+				userId: user._id.toString(),
+			});
+		}
 
-        res.status(200).json({
-            msg: "User saved",
-            accessToken: await generateAccessToken(userCreated),
-            refreshToken: refreshToken,
-            userId: userCreated._id.toString()
-        });
-
-    } catch (e) {
-        console.log(e)
-        const message = "Internal Server Error, Failed to Register User";
-        const details = e;
-        next({status: 411, message, details});
-    }
+		return res.status(401).send('Invalid Email or Password!');
+	} catch (e) {
+		console.log(e);
+		const message = 'Internal Server Error';
+		const details = e;
+		next({ message, details });
+	}
 };
 
-module.exports = { loginUser, registerUser, refreshTokenFunc, logoutUser, updateUserDetails };
+const registerUser = async (req, res, next) => {
+	try {
+		const { email } = req.body;
+
+		const userExist = await UserModel.findOne({ email });
+
+		if (userExist) {
+			return res.status(400).json({ msg: 'User already exists' });
+		}
+
+		const payload = { ...req.body };
+
+		const userCreated = await UserModel.create(payload);
+		const refreshToken = await generateRefreshToken(userCreated);
+
+		cacheFunc(refreshToken);
+
+		res.status(200).json({
+			msg: 'User saved',
+			accessToken: await generateAccessToken(userCreated),
+			refreshToken: refreshToken,
+			userId: userCreated._id.toString(),
+		});
+	} catch (e) {
+		console.log(e);
+		const message = 'Internal Server Error, Failed to Register User';
+		const details = e;
+		next({ status: 411, message, details });
+	}
+};
+
+module.exports = {
+	loginUser,
+	registerUser,
+	refreshTokenFunc,
+	logoutUser,
+	updateUserDetails,
+};
--- middleware/error-middleware.js
+++ middleware/error-middleware.js
@@ -1,7 +1,7 @@
 const errorMiddleware = (err, req, res, next) => {
-    const status = err.status | 500;
-    const message = err.message | "BackEnd Error";
-    const details = err.details | "Details Not Available";
+    const status = err.status || 500;
+    const message = err.message || "BackEnd Error";
+    const details = err.details || "Details Not Available";
 
     return res.status(status).json({message, details});
 }
